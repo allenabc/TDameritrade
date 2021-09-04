33f6aac92325358c04da34b95bf85425987ab99c
allenabc <allen_abc@yahoo.com>
2021-09-04 01:09:48 -0700

    first

---
+++ options.py
@@ -0,0 +1,24 @@
+from re import A
+import requests
+from requests.auth import HTTPBasicAuth
+from config import td_consumer_key
+import pprint
+url=r"https://api.tdameritrade.com/v1/marketdata/chains"
+
+symbol = 'AMD'
+
+strike = 100
+
+params = {
+    'symbol':symbol,
+    'strike':strike,
+    'apikey':td_consumer_key,
+    'fromDate':'2021-10-15',
+    'toDate':'2021-10-15',
+}
+#url = f"https://api.tdameritrade.com/v1/marketdata/AMD/quotes"
+results = requests.get(url, params=params).json()
+
+x = results['callExpDateMap']
+
+pprint.pprint(x)
